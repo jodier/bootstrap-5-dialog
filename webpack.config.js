82c3690ea841e28f455c4fe49afe7bd25e1ce699
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2021-04-03 10:51:52 +0200

    update

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+const BROWSER_LIST = [
+	">= 1%",
+	"last 1 major version",
+	"not dead",
+	"Chrome >= 45",
+	"Firefox >= 38",
+	"Edge >= 12",
+	"Explorer >= 10",
+	"iOS >= 9",
+	"Safari >= 9",
+	"Android >= 4.4",
+	"Opera >= 30"
+];
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+console.log("Building bootstrap-5-dialog for: " + BROWSER_LIST.join(", "));
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+module.exports = {
+	'entry': {
+		'bootstrap-5-dialog': path.resolve(__dirname, 'src/dialog.js'),
+		'bootstrap-5-dialog.min': path.resolve(__dirname, 'src/dialog.js')
+	},
+	'output': {
+		'path': path.resolve(__dirname, 'dist'),
+		'filename': '[name].js'
+	},
+	'module': {
+		'rules': [
+			/*--------------------------------------------------------------------------------------------------------*/
+
+			{
+				'test': /\.js$/,
+				'exclude': /node_modules/,
+				'use': {
+					'loader': 'babel-loader',
+					'options': {
+						'presets': [
+							['@babel/preset-env', {
+								'targets': BROWSER_LIST
+							}]
+						]
+					}
+				}
+			}
+
+			/*--------------------------------------------------------------------------------------------------------*/
+		]
+	},
+	'optimization': {
+		'minimizer': [
+			new TerserPlugin({
+				'test': /\.min\.js$/,
+				'parallel': true
+			})
+		]
+	}
+};
+
+/*--------------------------------------------------------------------------------------------------------------------*/
